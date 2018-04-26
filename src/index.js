a9ef18fae6a8fa4e4e051f07839fb21d590a030a
Koshkarik <koshkarevj@mail.ru>
2018-04-26 20:20:35 +0300

    change socket config

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const store = createStore(
 
 /* eslint-enable */
 
-const socket = io();
+const socket = io(window.location.hostname);
 socket.on('newMessage', (message) => {
   const { data: { attributes } } = message;
   store.dispatch(actions.addNewMessage(attributes));
